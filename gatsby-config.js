c9cd05a7e8528c3f9dc7cf671f8490638166467d
Pedro Duarte <contact@peduarte.com>
2019-01-26 15:20:54 +0000

    Add basic gatsby setup

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: 'JAMstack Cheatsheet',
+    author: 'Pedro Duarte',
+    description:
+      'A straight-to-the-point guide to help you get on the JAMstack.',
+    siteUrl: 'https://jamstack-cheatsheet.netlify.com',
+    social: {
+      twitter: '@peduarte',
+    },
+  },
+  pathPrefix: '/',
+};
