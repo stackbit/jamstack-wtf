b037aff9d675be19c2aeb72dcf103b9a94c6a011
Pedro Duarte <contact@peduarte.com>
2019-01-27 03:52:45 +0000

    Add icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { J, A, M } from '../icons';
+
+const colors = {
+  red: '#eb473d',
+  yellow: '#f0cd21',
+  green: '#4ca741',
+  blue: '#478cd4',
+  pink: '#e88ea4',
+};
 
 export default function() {
   return (
     <div>
+      <div>
+        <J width="60px" height="60px" style={{ color: colors.red }} />
+        <A width="60px" height="60px" style={{ color: colors.yellow }} />
+        <M width="60px" height="60px" style={{ color: colors.blue }} />
+      </div>
       <h1>JAMstack cheatsheet</h1>
       <p>A straight-to-the-point guide to help you get on the JAMstack.</p>
       <p>
