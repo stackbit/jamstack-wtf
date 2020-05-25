4ab0ede8a94034a3153a3b2bbcfd994fdb2773f3
Ondrej Polesny <ondrejp@kentico.com>
2020-05-25 16:34:42 +0200

    Add state of Jamstack 2020 report
    Create a new Report subsection and add the link to the State of Jamstack 2020 report.

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -166,6 +166,20 @@ export function Resources() {
             </Li>
           </Ul>
         </SubSection>
+    
+        <SubSection>
+          <SubSectionTitle>Report</SubSectionTitle>
+          <Ul>
+            <Li>
+              <Link
+                variant="ghost"
+                href="https://kontent.ai/resources/state-of-jamstack-2020-report"
+              >
+                The State of Jamstack 2020 Report
+              </Link>
+            </Li>
+          </Ul>
+        </SubSection>
 
         <SubSection>
           <SubSectionTitle>Community</SubSectionTitle>
