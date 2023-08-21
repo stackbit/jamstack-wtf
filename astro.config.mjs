b36408e02287f9777eae2bf0830974d3a215ce3f
TomasBankauskas <tbdizainas@gmail.com>
2023-08-21 10:34:53 +0300

    added GoogleAnalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
 	  integrations: [
+      partytown({
+        // Adds dataLayer.push as a forwarding-event.
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      }),
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
